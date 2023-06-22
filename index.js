585595872a474dbda92b8223a46c9e3d31499d9e
Geri Ang <gerillima@gmail.com>
2023-06-22 20:55:10 +0800

    integrating Whatsapp business endpoint updated

--- index.js
+++ index.js
@@ -84,45 +84,45 @@ App.use(express.urlencoded({
 }))
 
 
-// Handle verification
 App.get('/webhook', (req, res) => {
-    if (req.query['hub.mode'] === 'subscribe' &&
-        req.query['hub.verify_token'] === VERIFY_TOKEN) {
-        console.log("Validating webhook");
-        res.status(200).send(req.query['hub.challenge']);
-    } else {
-        console.error("Failed validation. Make sure the validation tokens match.");
-        res.sendStatus(403);          
-    }  
-});
-
-// Handle events
-App.post('/webhook', (req, res) => {
-    var data = req.body;
-    var signature = req.headers['x-hub-signature-256'];
-
-    // Verify the payload
-    var hash = crypto.createHmac('sha256', APP_SECRET)
-                     .update(JSON.stringify(data))
-                     .digest('hex');
-
-    if (hash === signature.substring(7)) {
-        // Process the webhook data
-        data.entry.forEach(function(pageEntry) {
-            var pageID = pageEntry.id;
-            var timeOfEvent = pageEntry.time;
-
-            // Iterate over each messaging event
-            pageEntry.changes.forEach(function(change) {
-                console.log("Webhook received: ", change);
-            });
-        });
-    } else {
-        console.error("Failed payload validation. Signature mismatch.");
+    // Parse the query params from the request
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+    
+    // Check if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
     }
-
-    res.sendStatus(200);
-});
+  });
+  
+  App.post('/webhook', (req, res) => {
+    const body = req.body;
+  
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+        // Gets the body of the webhook event
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  });
 
 
 App.post('/chatgpt', async (req, res) => {
