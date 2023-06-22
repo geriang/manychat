a114e3f86954dc58c8bfa25a9c4dff192453f661
Geri Ang <gerillima@gmail.com>
2023-06-22 18:42:37 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,117 @@
+const express = require("express");
+require('dotenv').config();
+const cors = require('cors');
+const App = express();
+
+// const { OpenAI } = require("langchain/llms/openai");
+const { ChatOpenAI } = require("langchain/chat_models/openai");
+// const { HumanChatMessage, SystemChatMessage } = require("langchain/schema");
+const {
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+} = require("langchain/prompts");
+const { ConversationChain } = require("langchain/chains");
+const { BufferWindowMemory } = require("langchain/memory");
+
+
+
+// const message = "do you know anything about Singapore Property?"
+
+// const reply = async () => {
+
+
+//     // initiating the chatmodel - openai
+//     const chat = new ChatOpenAI({ temperature: 0 });
+
+//     // defining the prompt templates
+//     const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+//         SystemMessagePromptTemplate.fromTemplate(`You are a helpful real estate agent bot from Huttons Sales & Auction.
+//         Your job is to answer to enquiries from both co-broke agents and prospective clients truthfully. 
+//         If there is any information that you cannot find, you have to refer the enquirer to contact Geri @ 84430486 for more information. 
+//         Always keep your reply to not more than 255 characters.
+//         At the start of an incoming enquiry, you need to determine the following:
+//         1. Who is the enquirer? Is the person a direct client or a co-broke agent?
+//         2. What is the nature of enquiry? Is it a sales enquiry, rental enquiry or general enquiry?
+//         3. Which property or property address is the enquirer enquirying on? 
+//         4. From where did the enquirer find the contact information to start the enquiry?`),
+//         HumanMessagePromptTemplate.fromTemplate("{input}"),
+//     ]);
+
+//     // initiating chain with memory function and chatprompt which introduces templates
+//     const chain = new ConversationChain({
+//         memory: new BufferWindowMemory({ k: 20 }),
+//         prompt: chatPrompt,
+//         llm: chat,
+//     });
+
+
+//     const response = await chain.call({
+//         input: "do you know anything about Singapore Property?",
+//     });
+
+//     console.log("response", response);
+// }
+
+// const startTemplate = `At the start of an incoming enquiry, you need to determine the following:
+// 1. Who is the enquirer? Is the person a direct client or a co-broke agent?
+// 2. What is the nature of enquiry? Is it a sales enquiry, rental enquiry or general enquiry?
+// 3. Which property or property address is the enquirer enquirying on? 
+// 4. From where did the enquirer find the contact information to start the enquiry?`
+
+// reply()
+
+
+
+App.use(cors({
+    origin: true
+}))
+
+App.use(express.urlencoded({
+    extended: false
+}))
+
+App.post('/chatgpt', async (req, res) => {
+    const message = req.body
+
+    // initiating the chatmodel - openai
+    const chat = new ChatOpenAI({ temperature: 0 });
+
+    // defining the prompt templates
+    const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+        SystemMessagePromptTemplate.fromTemplate(`You are a helpful real estate agent bot from Huttons Sales & Auction.
+         Your job is to answer to enquiries from both co-broke agents and prospective clients truthfully. 
+         If there is any information that you cannot find, you have to refer the enquirer to contact Geri @ 84430486 for more information. 
+         Always keep your reply to not more than 255 characters.
+         At the start of an incoming enquiry, you need to determine the following:
+         1. Who is the enquirer? Is the person a direct client or a co-broke agent?
+         2. What is the nature of enquiry? Is it a sales enquiry, rental enquiry or general enquiry?
+         3. Which property or property address is the enquirer enquirying on? 
+         4. From where did the enquirer find the contact information to start the enquiry?`),
+        HumanMessagePromptTemplate.fromTemplate("{input}"),
+    ]);
+
+    // initiating chain with memory function and chatprompt which introduces templates
+    const chain = new ConversationChain({
+        memory: new BufferWindowMemory({ k: 20 }),
+        prompt: chatPrompt,
+        llm: chat,
+    });
+
+
+    const response = await chain.call({
+        input: `${message}`,
+    });
+
+    res.send(response)
+})
+
+
+App.listen(process.env.PORT || 3000, () => {
+    console.log('server started')
+
+});
+
+
+
