61d7bcc3abc07676d0a1a389dab69916d0813af1
Geri Ang <gerillima@gmail.com>
2023-06-23 12:48:38 +0800

    integrating Whatsapp business endpoint updated 17

--- index.js
+++ index.js
@@ -37,51 +37,6 @@ const APP_SECRET = process.env.APP_SECRET;
 
 // const message = "do you know anything about Singapore Property?"
 
-// const reply = async () => {
-
-
-//     // initiating the chatmodel - openai
-//     const chat = new ChatOpenAI({ temperature: 0 });
-
-//     // defining the prompt templates
-//     const chatPrompt = ChatPromptTemplate.fromPromptMessages([
-//         SystemMessagePromptTemplate.fromTemplate(`You are a helpful real estate agent bot from Huttons Sales & Auction.
-//         Your job is to answer to enquiries from both co-broke agents and prospective clients truthfully. 
-//         If there is any information that you cannot find, you have to refer the enquirer to contact Geri @ 84430486 for more information. 
-//         Always keep your reply to not more than 255 characters.
-//         At the start of an incoming enquiry, you need to determine the following:
-//         1. Who is the enquirer? Is the person a direct client or a co-broke agent?
-//         2. What is the nature of enquiry? Is it a sales enquiry, rental enquiry or general enquiry?
-//         3. Which property or property address is the enquirer enquirying on? 
-//         4. From where did the enquirer find the contact information to start the enquiry?`),
-//         HumanMessagePromptTemplate.fromTemplate("{input}"),
-//     ]);
-
-//     // initiating chain with memory function and chatprompt which introduces templates
-//     const chain = new ConversationChain({
-//         memory: new BufferWindowMemory({ k: 20 }),
-//         prompt: chatPrompt,
-//         llm: chat,
-//     });
-
-
-//     const response = await chain.call({
-//         input: "do you know anything about Singapore Property?",
-//     });
-
-//     console.log("response", response);
-// }
-
-// const startTemplate = `At the start of an incoming enquiry, you need to determine the following:
-// 1. Who is the enquirer? Is the person a direct client or a co-broke agent?
-// 2. What is the nature of enquiry? Is it a sales enquiry, rental enquiry or general enquiry?
-// 3. Which property or property address is the enquirer enquirying on? 
-// 4. From where did the enquirer find the contact information to start the enquiry?`
-
-// reply()
-
-
-
 App.use(cors({
     origin: true
 }))
@@ -120,13 +75,24 @@ App.post('/webhook', async (req, res) => {
 
     });
 
+    // try {
+    //     console.log("Whatsapp message", message)
+    //     const data = {message}
+    //     let response = await axios.post("https://geriang-manychat.onrender.com/chatgpt", data) // await for the Promise to resolve
+    //     console.log("Response:", response); // then log the response
+    // } catch (err) {
+    //     console.error("Error sending message:", err);
+    // }
+
     try {
         console.log("Whatsapp message", message)
-        const data = {message}
-        let response = await axios.post("https://geriang-manychat.onrender.com/chatgpt", data) // await for the Promise to resolve
-        console.log("Response:", response); // then log the response
+        const data = { message }
+        let response = await axios.post("https://geriang-manychat.onrender.com/chatgpt", data).catch((error) => {
+            console.error("Error sending message:", error);
+        });
+        console.log("Response:", response);
     } catch (err) {
-        console.error("Error sending message:", err);
+        console.error("Error in POST /webhook:", err);
     }
 
     res.sendStatus(200);
@@ -164,9 +130,22 @@ App.post('/chatgpt', async (req, res) => {
     });
 
 
-    const response = await chain.call({
-        input: `${message}`,
-    });
+    // const response = await chain.call({
+    //     input: `${message}`,
+    // });
+    try {
+        console.log("Before chain.call");
+        const response = await chain.call({
+            input: `${message}`,
+        }).catch((error) => {
+            console.error("Error in chain.call:", error);
+        });
+        console.log("After chain.call");
+        console.log("ChatGPT Response", response)
+    } catch (err) {
+        console.error("Error in POST /chatgpt:", err);
+    }
+
 
     // res.send(response)
     console.log("ChatGPT Response", response)
