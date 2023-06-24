499c34c5362397a2db2f8138d1f2b9cd782d05a5
Geri Ang <gerillima@gmail.com>
2023-06-24 10:55:18 +0800

    changing memory component with vector store

--- index.js
+++ index.js
@@ -17,18 +17,17 @@ App.use(express.json()); // Middleware for parsing JSON bodies of incoming reque
 App.use(bodyParser.json());
 
 
-// const { OpenAI } = require("langchain/llms/openai");
 const { ChatOpenAI } = require("langchain/chat_models/openai");
-// const { HumanChatMessage, SystemChatMessage } = require("langchain/schema");
 const {
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
     PromptTemplate,
-    MessagesPlaceholder,
     SystemMessagePromptTemplate,
 } = require("langchain/prompts");
-const { ConversationChain } = require("langchain/chains");
-const { BufferMemory, BufferWindowMemory } = require("langchain/memory");
+const { ConversationChain, LLMChain } = require("langchain/chains");
+const { VectorStoreRetrieverMemory, BufferWindowMemory } = require("langchain/memory");
+const { MemoryVectorStore } = require("langchain/vectorstores/memory");
+const { OpenAIEmbeddings } = require("langchain/embeddings/openai");
 
 
 
@@ -45,15 +44,25 @@ App.use(express.urlencoded({
 App.post('/chatgpt', async (req, res) => {
 
 
-    const message = req.body.message
+    let message = req.body.message
     console.log("message received by chatgpt", message)
+
+    // *** initializing vector store and memory
+    const vectorStore = new MemoryVectorStore(new OpenAIEmbeddings());
+    const memory = new VectorStoreRetrieverMemory({
+        // 1 is how many documents to return, you might want to return more, eg. 4
+        vectorStoreRetriever: vectorStore.asRetriever(10),
+        memoryKey: "history",
+    });
+
+
 
     // initiating the chatmodel - openai
     const chat = new ChatOpenAI({ temperature: 0 });
 
     // defining the prompt templates
     const chatPrompt = ChatPromptTemplate.fromPromptMessages([
-        SystemMessagePromptTemplate.fromTemplate(`You are a helpful real estate agent bot from Huttons Sales & Auction.
+        SystemMessagePromptTemplate.fromTemplate(`You are a helpful real estate agent Bot from Huttons Sales & Auction.
          Your job is to answer to enquiries from both co-broke agents and prospective clients truthfully. 
          If there is any information that you cannot find, you have to refer the enquirer to contact Geri @ 84430486 for more information. 
          Always keep your reply to not more than 255 characters.
@@ -61,15 +70,20 @@ App.post('/chatgpt', async (req, res) => {
          1. Who is the enquirer? Is the person a direct client or a co-broke agent?
          2. What is the nature of enquiry? Is it a sales enquiry, rental enquiry or general enquiry?
          3. Which property or property address is the enquirer enquirying on? 
-         4. From where did the enquirer find the contact information to start the enquiry?`),
+         4. From where did the enquirer find the contact information to start the enquiry?
+         
+         Relevant pieces of previous conversation:
+         {history}
+         (You do not need to use these pieces of information if not relevant)
+
+         `),
         // new MessagesPlaceholder("history"),
         HumanMessagePromptTemplate.fromTemplate("{input}"),
     ]);
 
     // initiating chain with memory function and chatprompt which introduces templates
-    const chain = new ConversationChain({
-        // memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
-        memory: new BufferWindowMemory({ k: 20 }),
+    const chain = new LLMChain({
+        memory,
         prompt: chatPrompt,
         llm: chat,
     });
@@ -80,11 +94,18 @@ App.post('/chatgpt', async (req, res) => {
             input: `${message}`
         })
 
+        await memory.saveContext(
+            { input: `${message}` },
+            { output: `${response}` }
+        );
+
         res.send(response)
 
     } catch (err) {
         console.error("Error in POST /chatgpt:", err);
     }
+
+
 })
 
 App.listen(process.env.PORT || 3000, () => {
