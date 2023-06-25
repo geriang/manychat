cc6189a2b4be8d24e8bee7529e99ab607c0666ff
Geri Ang <gerillima@gmail.com>
2023-06-25 16:32:33 +0800

    debug multipromptchain 11

--- index.js
+++ index.js
@@ -34,9 +34,11 @@ const {
 } = require("langchain/prompts");
 const { LLMChain, LLMRouterChain, MultiPromptChain } = require("langchain/chains");
 const { RouterOutputParser } = require('langchain/output_parsers');
+const { z } = require("zod")
 const { ConversationChain } = require("langchain/chains");
 const { BufferMemory, ChatMessageHistory } = require("langchain/memory");
 const { HumanChatMessage, AIChatMessage } = require("langchain/schema");
+
 
 
 const { MongoClient } = require("mongodb");
@@ -107,20 +109,23 @@ App.post('/chatgpt', async (req, res) => {
 
     // defining the prompt templates
 
-    const promptNames = ["agent", "client", "general"]
-    const promptDescriptions = [ "Good for replying enquiry made by a real estate agent who wants to co-broke and wants information on a particular property", 
-    "Good for replying enquiries made by non real estate agents who wants information on a particular property",
-    "Good for replying generic enquiries"
-    ]
-
-    const agentTemplate = `You are a helpful real estate agent Bot from Huttons Sales & Auction.` +
+    let templates = [
+        {
+            name: 'agent',
+            description: 'Good for replying enquiry made by a real estate agent who wants to co-broke and wants information on a particular property',
+            template: `You are a helpful real estate agent Bot from Huttons Sales & Auction.` +
                 `Your job is to answer to enquiries of co-broke agents truthfully.` +
                 `If there is any information that you cannot find, you have to refer the enquirer to contact Geri @ 84430486 for more information.` +
                 `Always keep your reply to not more than 150 characters.` +
                 `At the start of an incoming enquiry, you need to determine the following:` +
                 `1. If the enquirer is a co-broke agent. If the enquirer is not, please switch to the clientTemplate as prompt.` +
                 `2. One of the ways to discern a co-broke agent is that the enquirer asks to co-broke or mentioned that they have a client who is interested in the property.`
-    const clientTemplate = `You are a helpful real estate agent Bot from Huttons Sales & Auction.` +
+
+        },
+        {
+            name: 'client',
+            description: 'Good for replying enquiries made by non real estate agents who wants information on a particular property',
+            template: `You are a helpful real estate agent Bot from Huttons Sales & Auction.` +
                 `Your job is to answer to enquiries of direct clients(non - real estate agent) truthfully.` +
                 `If there is any information that you cannot find, you have to refer the enquirer to contact Geri @ 84430486 for more information.` +
                 `Always keep your reply to not more than 200 characters.` +
@@ -128,7 +133,11 @@ App.post('/chatgpt', async (req, res) => {
                 `1. If the enquirer is a real estate agent, please switch to the agentTemplate as prompt.` +
                 `2. Ask for the enquirer's name and address the enquirer by his/her name if there is no record of his/her name.` +
                 `3. Ask for the enquirer's email address so that you could send more information to him/her regarding the property.`
-    const generalTemplate = `You are a helpful real estate agent Bot from Huttons Sales & Auction.` +
+        },
+        {
+            name: 'general',
+            description: 'Good for replying generic enquiries',
+            template: `You are a helpful real estate agent Bot from Huttons Sales & Auction.` +
                 `Your job is to answer to enquiries from both co-broke agents and prospective clients truthfully.` +
                 `If there is any information that you cannot find, you have to refer the enquirer to contact Geri @ 84430486 for more information.` +
                 `Always keep your reply to not more than 255 characters.` +
@@ -137,73 +146,33 @@ App.post('/chatgpt', async (req, res) => {
                 `2. What is the nature of enquiry? Is it a sales enquiry, rental enquiry or general enquiry?` +
                 `3. Which property or property address is the enquirer enquirying on?` +
                 `4. From where did the enquirer find the contact information to start the enquiry?`
-
-    const promptTemplates = [agentTemplate, clientTemplate, generalTemplate]
-
-    // let templates = [
-    //     {
-    //         name: 'agent',
-    //         description: 'Good for replying enquiry made by a real estate agent who wants to co-broke and wants information on a particular property',
-    //         template: `You are a helpful real estate agent Bot from Huttons Sales & Auction.` +
-    //             `Your job is to answer to enquiries of co-broke agents truthfully.` +
-    //             `If there is any information that you cannot find, you have to refer the enquirer to contact Geri @ 84430486 for more information.` +
-    //             `Always keep your reply to not more than 150 characters.` +
-    //             `At the start of an incoming enquiry, you need to determine the following:` +
-    //             `1. If the enquirer is a co-broke agent. If the enquirer is not, please switch to the clientTemplate as prompt.` +
-    //             `2. One of the ways to discern a co-broke agent is that the enquirer asks to co-broke or mentioned that they have a client who is interested in the property.`
-
-    //     },
-    //     {
-    //         name: 'client',
-    //         description: 'Good for replying enquiries made by non real estate agents who wants information on a particular property',
-    //         template: `You are a helpful real estate agent Bot from Huttons Sales & Auction.` +
-    //             `Your job is to answer to enquiries of direct clients(non - real estate agent) truthfully.` +
-    //             `If there is any information that you cannot find, you have to refer the enquirer to contact Geri @ 84430486 for more information.` +
-    //             `Always keep your reply to not more than 200 characters.` +
-    //             `At the start of an incoming enquiry, you need to determine the following:` +
-    //             `1. If the enquirer is a real estate agent, please switch to the agentTemplate as prompt.` +
-    //             `2. Ask for the enquirer's name and address the enquirer by his/her name if there is no record of his/her name.` +
-    //             `3. Ask for the enquirer's email address so that you could send more information to him/her regarding the property.`
-    //     },
-    //     {
-    //         name: 'general',
-    //         description: 'Good for replying generic enquiries',
-    //         template: `You are a helpful real estate agent Bot from Huttons Sales & Auction.` +
-    //             `Your job is to answer to enquiries from both co-broke agents and prospective clients truthfully.` +
-    //             `If there is any information that you cannot find, you have to refer the enquirer to contact Geri @ 84430486 for more information.` +
-    //             `Always keep your reply to not more than 255 characters.` +
-    //             `At the start of an incoming enquiry, you need to determine the following:` +
-    //             `1. Who is the enquirer? Is the person a direct client or a co-broke agent?` +
-    //             `2. What is the nature of enquiry? Is it a sales enquiry, rental enquiry or general enquiry?` +
-    //             `3. Which property or property address is the enquirer enquirying on?` +
-    //             `4. From where did the enquirer find the contact information to start the enquiry?`
-    //     }
-    // ];
+        }
+    ];
 
 
     // Build an array of destination LLMChains and a list of the names with descriptions
-    // let destinationChains = {};
+    let destinationChains = {};
 
-    // for (const item of templates) {
-    //     let prompt = ChatPromptTemplate.fromPromptMessages([SystemMessagePromptTemplate.fromTemplate(`${item.template}`),
-    //     new MessagesPlaceholder("chat_history"),
-    //     HumanMessagePromptTemplate.fromTemplate("{input}")]);
-    //     let chain = new ConversationChain({
-    //         prompt: prompt,
-    //         memory: new BufferMemory({
-    //             chatHistory: new ChatMessageHistory(pastMessages),
-    //             returnMessages: true,
-    //             memoryKey: "chat_history"
-    //         }),
-    //         llm: llm
-    //     });
-    //     destinationChains[item.name] = chain;
-    // }
+    for (const item of templates) {
+        let prompt = ChatPromptTemplate.fromPromptMessages([SystemMessagePromptTemplate.fromTemplate(`${item.template}`),
+        new MessagesPlaceholder("chat_history"),
+        HumanMessagePromptTemplate.fromTemplate("{input}")]);
+        let chain = new ConversationChain({
+            prompt: prompt,
+            memory: new BufferMemory({
+                chatHistory: new ChatMessageHistory(pastMessages),
+                returnMessages: true,
+                memoryKey: "chat_history"
+            }),
+            llm: llm
+        });
+        destinationChains[item.name] = chain;
+    }
 
-    // let destinations = templates.map(item => (item.name + ': ' + item.description)).join('\n');
+    let destinations = templates.map(item => (item.name + ': ' + item.description)).join('\n');
 
     // Create a default destination in case the LLM cannot decide
-    const defaultPrompt = ChatPromptTemplate.fromPromptMessages([
+    let defaultPrompt = ChatPromptTemplate.fromPromptMessages([
         SystemMessagePromptTemplate.fromTemplate(`You are a helpful real estate agent Bot from Huttons Sales & Auction.
          Your job is to answer to enquiries from both co-broke agents and prospective clients truthfully. 
          If there is any information that you cannot find, you have to refer the enquirer to contact Geri @ 84430486 for more information. 
@@ -218,7 +187,7 @@ App.post('/chatgpt', async (req, res) => {
         HumanMessagePromptTemplate.fromTemplate("{input}"),
     ]);
 
-    const defaultChain = new ConversationChain({
+    let defaultChain = new ConversationChain({
         llm: llm,
         prompt: defaultPrompt,
         memory: new BufferMemory({
@@ -229,76 +198,72 @@ App.post('/chatgpt', async (req, res) => {
     });
 
     // Now set up the router and it's template
-    // let routerTemplate = 'Given a raw text input to a ' +
-    //     'language model select the model prompt best suited for the input. ' +
-    //     'You will be given the names of the available prompts and a ' +
-    //     'description of what the prompt is best suited for. ' +
-    //     'You may also revise the original input if you think that revising ' +
-    //     'it will ultimately lead to a better response from the language model.\n\n' +
-    //     '<< FORMATTING >>\n' +
-    //     'Return a markdown code snippet with a JSON object formatted to look like:\n' +
-    //     '```json\n' +
-    //     '{{\n' +
-    //     '    "destination": string, // name of the prompt to use or "DEFAULT"\n' +
-    //     '    "next_inputs": string // a potentially modified version of the original input\n' +
-    //     '}}\n' +
-    //     '```\n\n' +
-    //     'REMEMBER: "destination" MUST be one of the candidate prompt ' +
-    //     'names specified below OR it can be "DEFAULT" if the input is not ' +
-    //     'well suited for any of the candidate prompts. ' +
-    //     'REMEMBER: "next_inputs" can just be the original input ' +
-    //     'if you don\'t think any modifications are needed.\n\n' +
-    //     '<< CANDIDATE PROMPTS >>\n' +
-    //     '{destinations}\n\n' +
-    //     '<< INPUT >>\n' +
-    //     '{input}\n\n' +
-    //     '<< OUTPUT (remember to include the ```json)>>';
+    let routerTemplate = 'Given a raw text input to a ' +
+        'language model select the model prompt best suited for the input. ' +
+        'You will be given the names of the available prompts and a ' +
+        'description of what the prompt is best suited for. ' +
+        'You may also revise the original input if you think that revising ' +
+        'it will ultimately lead to a better response from the language model.\n\n' +
+        '<< FORMATTING >>\n' +
+        'Return a markdown code snippet with a JSON object formatted to look like:\n' +
+        '```json\n' +
+        '{{\n' +
+        '    "destination": string, // name of the prompt to use or "DEFAULT"\n' +
+        '    "next_inputs": string // a potentially modified version of the original input\n' +
+        '}}\n' +
+        '```\n\n' +
+        'REMEMBER: "destination" MUST be one of the candidate prompt ' +
+        'names specified below OR it can be "DEFAULT" if the input is not ' +
+        'well suited for any of the candidate prompts. ' +
+        'REMEMBER: "next_inputs" can just be the original input ' +
+        'if you don\'t think any modifications are needed.\n\n' +
+        '<< CANDIDATE PROMPTS >>\n' +
+        '{destinations}\n\n' +
+        '<< INPUT >>\n' +
+        '{input}\n\n' +
+        '<< OUTPUT (remember to include the ```json)>>';
+
+    // Now we can construct the router with the list of route names and descriptions
+    routerTemplate = routerTemplate.replace('{destinations}', destinations);
 
     // ***
-    // let routerParser = RouterOutputParser.fromNamesAndDescriptions({
-    //     destination: 'name of the prompt to use or "DEFAULT"',
-    //     next_inputs: 'a potentially modified version of the original input'
-    // });
+    let routerParser = RouterOutputParser.fromZodSchema(z.object({
+        destination: z
+            .string()
+            .describe('name of the prompt to use or "DEFAULT"'),
+        next_inputs: z.object({
+            input: z
+                .string()
+                .describe('a potentially modified version of the original input')
+        })
+    }))
+
+    let routerFormat = routerParser.getFormatInstructions();
 
     // ***
 
     // Now we can construct the router with the list of route names and descriptions
-    // routerTemplate = routerTemplate.replace('{destinations}', destinations);
 
-    // let routerPrompt = new PromptTemplate({
-    //     template: routerTemplate,
-    //     inputVariables: ['input'],
-    //     outputParser: routerParser
-    // });
+    let routerPrompt = new PromptTemplate({
+        template: routerTemplate,
+        inputVariables: ['input'],
+        outputParser: routerParser,
+        partialVariables: {
+            format_instructions: routerFormat
+        }
+    });
 
-    // let routerChain = LLMRouterChain.fromLLM(llm, routerPrompt);
+    let routerChain = LLMRouterChain.fromLLM(llm, routerPrompt);
 
     // Now we can bring all of the pieces together!
-
-    const multiPromptChain = MultiPromptChain.fromLLMAndPrompts(llm, {
-        defaultChain: defaultChain,
-        promptNames,
-        promptDescriptions,
-        promptTemplates,
-      });
-
-    // let multiPromptChain = new MultiPromptChain({
-    //     routerChain,
-    //     destinationChains,
-    //     defaultChain,
-    //     verbose: true
-    // });
+    let multiPromptChain = new MultiPromptChain({
+        routerChain,
+        destinationChains,
+        defaultChain,
+        verbose: true
+    });
 
     console.log("multiPromptChain", multiPromptChain)
-
-
-    // const promptTemplates = [physicsTemplate, mathTemplate, historyTemplate];
-
-    // const multiPromptChain = MultiPromptChain.fromLLMAndPrompts(llm, {
-    //     promptNames,
-    //     promptDescriptions,
-    //     promptTemplates,
-    // });
 
 
     // initiating chain with memory function and chatprompt which introduces templates
