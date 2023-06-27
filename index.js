cdcbd6fbd3185a659f642e73b2e459e9f0a20755
Geri Ang <gerillima@gmail.com>
2023-06-27 18:34:13 +0800

    integrated whatsapp cloud api debug 4

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ App.post('/webhook', async (req, res) => {
     // Handle text message
     let message = data.entry[0].changes[0].value.messages[0].text.body;
     let phone_number = data.entry[0].changes[0].value.metadata.phone_number_id;
-    let from = data.entry[0].changes[0].value.message[0].from;
+    // let from = data.entry[0].changes[0].value.message[0].from;
 
     try {
         const data = { message, "whatsapp_id": phone_number }
