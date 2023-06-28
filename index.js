77a42c815f6178bc5c729ad8921d18e98df018f0
Geri Ang <gerillima@gmail.com>
2023-06-28 21:07:28 +0800

    testing prompts 10

--- index.js
+++ index.js
@@ -186,12 +186,10 @@ App.post('/chatgpt', async (req, res) => {
             name: "chatting_tool",
             description:
                 "use this tool to simply chat with human.",
-            func: async () =>{
+            func: async (input) =>{
                 const chat = new ChatOpenAI();
                 await chat.call([
-                  new HumanChatMessage(
-                    `${message}`
-                  ),
+                  new HumanChatMessage(input),
                 ]);
             },
             returnDirect: true
