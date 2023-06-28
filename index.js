717e0f09a1174d0629debe9838050c3f37959897
Geri Ang <gerillima@gmail.com>
2023-06-28 19:20:43 +0800

    debug whatsapp webhook 29

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ App.post('/chatgpt', async (req, res) => {
     // console.log("past messages", pastMessages)
 
     // initiating the chatmodel - openai
-    const llm = new ChatOpenAI({ temperature: 0, modelName:"GPT-4" });
+    const llm = new ChatOpenAI({ temperature: 0 });
 
     //  to embed property listing information
     /* Load in the file we want to do question answering over */
