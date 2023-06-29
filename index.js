847f04dc20acc8f61f95e8e92b301b296b9c2f8b
Geri Ang <gerillima@gmail.com>
2023-06-29 12:32:15 +0800

    testing model 13

--- index.js
+++ index.js
@@ -222,7 +222,7 @@ App.post('/chatgpt', async (req, res) => {
         }
     });
 
-    console.log("Check template", executor.agent.StructuredChatAgent.llmChain.prompt)
+    console.log("Check template", executor.agent.llmChain.prompt)
 
     try {
         const version = process.env.WHATSAPP_VERSION
