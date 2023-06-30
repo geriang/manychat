a7e79cd462ad1e6b39e8beb686ee5fa146431f9b
Geri Ang <gerillima@gmail.com>
2023-06-30 20:48:46 +0800

    debug 6

--- index.js
+++ index.js
@@ -228,8 +228,8 @@ App.post('/chatgpt', async (req, res) => {
             memoryPrompts: [new MessagesPlaceholder("chat_history")],
             // prefix: "You are a chatbot that answers to enquires. Ask for the person's name if it is unknown. If the name is known, greet the person by name.",
             // prefix: "Remember to STRICTLY use the following format: Question, Thought, Action, Auction Input, Observation, Thought, Final Answer. DO NOT SKIP ANY OF THE STEPS AT ALL TIMES",
-            suffix: "Politely asks for a name if you do not know the person's name."
-            // prefix: "If the user's name is not known, ask for the name politely."
+            // suffix: "Politely asks for a name if you do not know the person's name."
+            prefix: "You are a chatbot that answers to enquires and ask for the user's name politely if it is not known."
         }
     });
 
