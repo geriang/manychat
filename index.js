d2acb6871a414d24c03ddcb4f9b272ef2380ca2e
Geri Ang <gerillima@gmail.com>
2023-06-30 18:53:12 +0800

    reintroducing memory 23

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ App.post('/chatgpt', async (req, res) => {
         agentArgs: {
             inputVariables: ["input", "agent_scratchpad", "chat_history"],
             memoryPrompts: [new MessagesPlaceholder("chat_history")],
-            prefix: "You are a chatbot that answers to enquires. Ask for the name if it is not found in chat history or chat record. If a name is found, greet the person by name.",
+            prefix: "You are a chatbot that answers to enquires. Always ask for a name if it is not found in chat history or chat record. If a name is found, greet the person by name.",
             // prefix: "Remember to STRICTLY use the following format: Question, Thought, Action, Auction Input, Observation, Thought, Final Answer. DO NOT SKIP ANY OF THE STEPS AT ALL TIMES",
             // suffix: "Remember to STRICTLY use the following format: Question, Thought, Action, Auction Input, Observation, Thought, Final Answer. DO NOT SKIP ANY OF THE STEPS AT ALL TIMES"
         }
