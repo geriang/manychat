eb78df728df5f8a5e4a887d6c5b1922ecd98038f
Geri Ang <gerillima@gmail.com>
2023-06-30 13:37:15 +0800

    reintroducing memory 10

--- index.js
+++ index.js
@@ -149,8 +149,8 @@ App.post('/chatgpt', async (req, res) => {
 
     if (pastMessagesData) {
         pastMessages = [
-            new HumanChatMessage((pastMessagesData.map((obj) => { return obj.client })).toString())
-            // new AIChatMessage((pastMessagesData.map((obj) => { return obj.bot })).toString())
+            new HumanChatMessage((pastMessagesData.map((obj) => { return obj.client })).toString()),
+            new AIChatMessage("")
         ]
     }
 
