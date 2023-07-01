98f1012fe73f5980cccc8f66af812f1db2118712
Geri Ang <gerillima@gmail.com>
2023-07-01 16:32:04 +0800

    try return intermediate again

--- index.js
+++ index.js
@@ -218,8 +218,8 @@ App.post('/chatgpt', async (req, res) => {
         agentType: "structured-chat-zero-shot-react-description",
         verbose: true,
         maxIterations: 5,
-        earlyStoppingMethod: "generate",
-        // returnIntermediateSteps: true,
+        // earlyStoppingMethod: "generate",
+        returnIntermediateSteps: true,
         memory: new BufferMemory({
             chatHistory: new ChatMessageHistory(pastMessages),
             returnMessages: true,
