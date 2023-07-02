79fb33b6f79db784fd2b105160b060bd82582b6f
Geri Ang <gerillima@gmail.com>
2023-07-02 13:37:30 +0800

    modifying system prompt 8

--- index.js
+++ index.js
@@ -226,20 +226,20 @@ App.post('/chatgpt', async (req, res) => {
     });
 
     console.log("Check template", executor.agent.llmChain.prompt.promptMessages[0].prompt.template)
-    let prompt = `Answer the following questions truthfully and as best you can.
+    let prompt = `Check template Answer the following questions truthfully and as best you can.
 
     You have access to the following tools.
     You must format your inputs to these tools to match their "JSON schema" definitions below.
     
     "JSON Schema" is a declarative language that allows you to annotate and validate JSON documents.
     
-    For example, the example "JSON Schema" instance {{"properties": {{"foo": {{"description": "a list of test words", "type": "array", "items": {{"type": "string"}}}}}}, "required": ["foo"]}}}}
+    For example, the example "JSON Schema" instance {{{"properties": {{"foo": {{"description": "a list of test words", "type": "array", "items": {{"type": "string"}}}}}}, "required": ["foo"]}}}}
     would match an object with one required property, "foo". The "type" property specifies "foo" must be an "array", and the "description" property semantically describes it as "a list of test words". The items within "foo" must be strings.
     Thus, the object {{"foo": ["bar", "baz"]}} is a well-formatted instance of this example "JSON Schema". The object {{"properties": {{"foo": ["bar", "baz"]}}}} is not well-formatted.
     
     Here are the JSON Schema instances for the tools you have access to:
     
-    {{tool_schemas}}
+    {tool_schemas}
     
     The way you use the tools is as follows:
     
@@ -248,15 +248,15 @@ App.post('/chatgpt', async (req, res) => {
     Output a JSON markdown code snippet containing a valid JSON blob (denoted below by $JSON_BLOB).
     This $JSON_BLOB must have a "action" key (with the name of the tool to use) and an "action_input" key (tool input).
     
-    Valid "action" values: "Final Answer" (which you must use when giving your final response to the user), or one of [{{tool_names}}].
+    Valid "action" values: "Final Answer" (which you must use when giving your final response to the user), or one of [{tool_names}].
     
     The $JSON_BLOB must be valid, parseable JSON and only contain a SINGLE action. Here is an example of an acceptable output:
     
     \`\`\`json
-    {
-      "action": {{TOOL_NAME}},
-      "action_input": {{INPUT}}
-    }
+    {{
+      "action": $TOOL_NAME,
+      "action_input": $INPUT
+    }}
     \`\`\`
     
     Remember to include the surrounding markdown code snippet delimiters (begin with "\`\`\`" json and close with "\`\`\`")!
@@ -278,12 +278,14 @@ App.post('/chatgpt', async (req, res) => {
     Thought: I now know the final answer
     Action:
     \`\`\`json
-    {
+    {{
       "action": "Final Answer",
       "action_input": "Final response to human"
-    }
+    }}
     \`\`\`
-    `;
+    
+    Begin! Reminder to ALWAYS use the above format, and to use tools if appropriate.`;
+    
 
     executor.agent.llmChain.prompt.promptMessages[0].prompt.template = prompt
 
