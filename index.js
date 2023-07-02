fbfec274a85239de3973ded14d2737bf3de6fe44
Geri Ang <gerillima@gmail.com>
2023-07-02 16:47:40 +0800

    modifying system prompt 19

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ App.post('/chatgpt', async (req, res) => {
 
     // console.log("Check template", executor.agent.llmChain.prompt.promptMessages[0].prompt.template)
     let prompt = 
-    `You are a chatbot that answers to the following question truthfully and as best you can.`+
+    `Answer the following questions truthfully and as best you can.`+
     `You have access to the following tools.`+
     `You must format your inputs to these tools to match their "JSON schema" definitions below.`+
 
