baab374ee1462107e4c056f53e854f2bca6558a1
Geri Ang <gerillima@gmail.com>
2023-07-03 19:24:53 +0800

    reset

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ App.post('/chatgpt', async (req, res) => {
     // console.log("past messages", pastMessages)
 
     // initiating the chatmodel - openai
-    const llm = new ChatOpenAI({ modelName: "gpt-3.5-turbo-0613", temperature: 0.0 });
+    const llm = new ChatOpenAI({ modelName: "gpt-3.5-turbo-0613", temperature: 0 });
     const embeddings = new OpenAIEmbeddings();
 
     //  to embed property listing information
@@ -193,18 +193,18 @@ App.post('/chatgpt', async (req, res) => {
             func: async (input) => `${input}`,
             returnDirect: true
         }),
-        // new SerpAPI(`${process.env.SERPAPI_API_KEY}`, {
-        //     location: "Singapore",
-        //     hl: "en",
-        //     gl: "sg",
-        // }),
+        new SerpAPI(`${process.env.SERPAPI_API_KEY}`, {
+            location: "Singapore",
+            hl: "en",
+            gl: "sg",
+        }),
         new WebBrowser({ llm, embeddings }),
         propertyDatabaseTool,
     ];
 
     // initialize the agent
     const executor = await initializeAgentExecutorWithOptions(tools, llm, {
-        agentType: "chat-conversational-react-description",
+        agentType: "structured-chat-zero-shot-react-description",
         verbose: true,
         maxIterations: 5,
         // earlyStoppingMethod: "force",
@@ -215,81 +215,25 @@ App.post('/chatgpt', async (req, res) => {
             returnMessages: true,
         }),
         agentArgs: {
-            // inputVariables: ["input", "agent_scratchpad", "chat_history"],
+            inputVariables: ["input", "agent_scratchpad", "chat_history"],
             memoryPrompts: [new MessagesPlaceholder({ variableName: "chat_history" })],
             // prefix: "You are a chatbot that answers to enquires. Ask for the person's name if it is unknown. If the name is known, greet the person by name.",
             // prefix: "Remember to STRICTLY use the following format: Question, Thought, Action, Auction Input, Observation, Thought, Final Answer. DO NOT SKIP ANY OF THE STEPS AT ALL TIMES",
+            // suffix: "Politely asks for a name if you do not know the person's name."
+            // suffix: "You are a chatbot that answers to enquires and ask for the user's name politely if it is not known."
             // suffix: "You are a chatbot that answers to enquires. Always ask for the name if it is not found in chat history. If a name is found, greet the person by name.",
         }
     });
 
     // console.log("Check template", executor.agent.llmChain.prompt.promptMessages[0].prompt.template)
-    // let prompt = 
-    // `Answer the following questions truthfully and as best you can.`+
-    // `You have access to the following tools.`+
-    // `You must format your inputs to these tools to match their "JSON schema" definitions below.`+
+    // let prompt = 'You are a chatbot'
+    // executor.agent.llmChain.prompt.promptMessages[0].prompt.template=prompt
 
-    // `"JSON Schema" is a declarative language that allows you to annotate and validate JSON documents.`+
-    
-    // `For example, the example "JSON Schema" instance {{"properties": {{"foo": {{"description": "a list of test words", "type": "array", "items": {{"type": "string"}}}}}}, "required": ["foo"]}}`+
-    // `would match an object with one required property, "foo". The "type" property specifies "foo" must be an "array", and the "description" property semantically describes it as "a list of test words". The items within "foo" must be strings.`+
-    // `Thus, the object {{"foo": ["bar", "baz"]}} is a well-formatted instance of this example "JSON Schema". The object {{"properties": {{"foo": ["bar", "baz"]}}}} is not well-formatted.`+
-    
-    // `Here are the JSON Schema instances for the tools you have access to:`+
-    
-    // `{tool_schemas}`+
-    
-    // `The way you use the tools is as follows:`+
-    
-    // `------------------------`+
-    
-    // `Output a JSON markdown code snippet containing a valid JSON blob (denoted below by $JSON_BLOB).`+
-    // `This $JSON_BLOB must have a "action" key (with the name of the tool to use) and an "action_input" key (tool input).`+
-    
-    // `Valid "action" values: "Final Answer" (which you must use when giving your final response to the user), or one of [{tool_names}].`+
-    
-    // `The $JSON_BLOB must be valid, parseable JSON and only contain a SINGLE action. Here is an example of an acceptable output:`+
-    
-    // `\`\`\`json`+
-    // `{{`+
-    //   `"action": $TOOL_NAME,`+
-    //   `"action_input": $INPUT`+
-    // `}}`+
-    // `\`\`\``+
-    
-    // `Remember to include the surrounding markdown code snippet delimiters (begin with "\`\`\`" json and close with "\`\`\`")!`+
-    
-    // `If you are using a tool, "action_input" must adhere to the tool's input schema, given above.`+
-    
-    // `------------------------`+
-    
-    // `ALWAYS use the following format:`+
-    
-    // `Question: the input question you must answer`+
-    // `Thought: you should always think about what to do`+
-    // `Action:`+
-    // `\`\`\`json`+
-    // `$JSON_BLOB`+
-    // `\`\`\``+
-    // `Observation: the result of the action`+
-    // `... (this Thought/Action/Observation can repeat N times)`+
-    // `Thought: I now know the final answer`+
-    // `Action:`+
-    // `\`\`\`json`+
-    // `{{`+
-    //   `"action": "Final Answer",`+
-    //   `"action_input": "Final response to human"`+
-    // `}}`+
-    // `\`\`\``+
-    
-    // `Begin! Reminder to ALWAYS use the above format, and to use tools if appropriate.`;
-
-    // executor.agent.llmChain.prompt.promptMessages[0].prompt.template = prompt
 
     try {
         const version = process.env.WHATSAPP_VERSION
         const phoneNumberID = process.env.WHATSAPP_PHONE_NUMBER_ID
-        const response = await executor.call({ input: `${message} ` });
+        const response = await executor.call({ input: `${message}` });
         console.log("response", response)
 
         await axios.post(`https://graph.facebook.com/${version}/${phoneNumberID}/messages`, {
