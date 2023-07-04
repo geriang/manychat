79943d7478be6f987a86c5dae10a743c81193a7d
Geri Ang <gerillima@gmail.com>
2023-07-04 19:32:18 +0800

    creating my own chain 11

--- api/chat.js
+++ api/chat.js
@@ -56,7 +56,7 @@ router.post('/', async (req, res) => {
     const chatPrompt = ChatPromptTemplate.fromPromptMessages([
         SystemMessagePromptTemplate.fromTemplate(
         `You are a friendly chatbot from Huttons Sales & Auction in Singapore.`+
-        `Always ask for the customer name if it is not found in {chat_history}. If a name is found, greet the person by name.`
+        `Always ask for the customer name if it is not found in previous chat conversations. If a name is found, greet the person by name.`
          ),
         new MessagesPlaceholder("chat_history"),
         HumanMessagePromptTemplate.fromTemplate("{input}"),
