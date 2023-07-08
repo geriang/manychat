c9777f264a1948ba2c557c43fb12fec966af08f8
Geri Ang <gerillima@gmail.com>
2023-07-08 11:50:05 +0800

    added tiggerchat as middleware

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const bodyParser = require('body-parser');
 const App = express();
 const whatsappEndpoint = require("./api/whatsapp")
 const chatEndpoint = require("./api/chat")
+const triggerChat = require("./middleware/chatTrigger")
 
 App.use(express.json()); // Middleware for parsing JSON bodies of incoming requests
 App.use(bodyParser.json());
@@ -25,14 +26,46 @@ App.use(express.urlencoded({
 }))
 
 
+
 App.use('/whatsapp', whatsappEndpoint);
-App.use('/chatgpt', chatEndpoint)
+App.use('/chatgpt', triggerChat, chatEndpoint)
 
 
 App.listen(process.env.PORT || 3000, () => {
     console.log('server started')
 
 });
+
+
+
+// const express = require('express');
+// const app = express();
+
+// // In-memory store for the last function call timestamp.
+// let lastFunctionCallTimestamp = null;
+
+// // Your middleware function.
+// const rateLimit = (req, res, next) => {
+//     const currentTime = Date.now();
+//     const sixHoursInMilliseconds = 6 * 60 * 60 * 1000;
+
+//     if (lastFunctionCallTimestamp && currentTime - lastFunctionCallTimestamp < sixHoursInMilliseconds) {
+//         return res.status(429).send("You can only trigger this function once every 6 hours.");
+//     }
+
+//     lastFunctionCallTimestamp = currentTime;
+//     next();
+// };
+
+// app.use(rateLimit);
+
+// app.get('/', function (req, res) {
+//     res.send('This function is called!');
+// });
+
+// app.listen(3000, function () {
+//     console.log('App is listening on port 3000');
+// });
 
 
 
