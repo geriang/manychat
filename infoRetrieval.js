913d727bc5889785c395781270c3fb3b93b1b2a0
Geri Ang <gerillima@gmail.com>
2023-07-11 19:00:31 +0800

    added name extractor debug 2

--- infoRetrieval.js
+++ infoRetrieval.js
@@ -44,37 +44,37 @@ module.exports = findName
 
 
 
-// This is an LLMChain to write a synopsis given a title of a play.
-const llm = new OpenAI({ temperature: 0 });
-const template = `You are a playwright. Given the title of play, it is your job to write a synopsis for that title.
+// // This is an LLMChain to write a synopsis given a title of a play.
+// const llm = new OpenAI({ temperature: 0 });
+// const template = `You are a playwright. Given the title of play, it is your job to write a synopsis for that title.
  
-  Title: {title}
-  Playwright: This is a synopsis for the above play:`;
-const promptTemplate = new PromptTemplate({
-    template,
-    inputVariables: ["title"],
-});
-const synopsisChain = new LLMChain({ llm, prompt: promptTemplate });
+//   Title: {title}
+//   Playwright: This is a synopsis for the above play:`;
+// const promptTemplate = new PromptTemplate({
+//     template,
+//     inputVariables: ["title"],
+// });
+// const synopsisChain = new LLMChain({ llm, prompt: promptTemplate });
 
-// This is an LLMChain to write a review of a play given a synopsis.
-const reviewLLM = new OpenAI({ temperature: 0 });
-const reviewTemplate = `You are a play critic from the New York Times. Given the synopsis of play, it is your job to write a review for that play.
+// // This is an LLMChain to write a review of a play given a synopsis.
+// const reviewLLM = new OpenAI({ temperature: 0 });
+// const reviewTemplate = `You are a play critic from the New York Times. Given the synopsis of play, it is your job to write a review for that play.
  
-  Play Synopsis:
-  {synopsis}
-  Review from a New York Times play critic of the above play:`;
-const reviewPromptTemplate = new PromptTemplate({
-    template: reviewTemplate,
-    inputVariables: ["synopsis"],
-});
-const reviewChain = new LLMChain({
-    llm: reviewLLM,
-    prompt: reviewPromptTemplate,
-});
+//   Play Synopsis:
+//   {synopsis}
+//   Review from a New York Times play critic of the above play:`;
+// const reviewPromptTemplate = new PromptTemplate({
+//     template: reviewTemplate,
+//     inputVariables: ["synopsis"],
+// });
+// const reviewChain = new LLMChain({
+//     llm: reviewLLM,
+//     prompt: reviewPromptTemplate,
+// });
 
-const overallChain = new SimpleSequentialChain({
-    chains: [synopsisChain, reviewChain],
-    verbose: true,
-});
-const review = await overallChain.run("Tragedy at sunset on the beach");
-console.log(review);
+// const overallChain = new SimpleSequentialChain({
+//     chains: [synopsisChain, reviewChain],
+//     verbose: true,
+// });
+// const review = await overallChain.run("Tragedy at sunset on the beach");
+// console.log(review);
