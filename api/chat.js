239f6c648d81ffd1e147a2f560645f4551f1d4ea
Geri Ang <gerillima@gmail.com>
2023-07-14 22:59:30 +0800

    added request email feature added validation and debug 3

--- api/chat.js
+++ api/chat.js
@@ -77,7 +77,7 @@ router.post('/', async (req, res) => {
         const emailCheck = email.includes("<")
         if (emailCheck) {
             // console.log("FIND NAME EXTRACTED", name)
-            const modifiedEmail = name.replace(/<|>|\s/g, "");
+            const modifiedEmail = email.replace(/<|>|\s/g, "");
             console.log("modified email address is", modifiedEmail)
             // await addName(whatsapp_id, modifiedName)
         }
